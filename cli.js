72dfd049f748bb2fea9cbf267d9255c3c5b901d7
Andrea Parodi <andrea@parro.it>
2016-06-18 18:44:26 +0200

    fix style on main files

--- cli.js
+++ cli.js
@@ -1,14 +1,17 @@
 #!/usr/bin/env node
-var download = require('./')
-var minimist = require('minimist')
-var opts = minimist(process.argv.slice(2))
+var minimist = require('minimist');
+var download = require('./');
+
+var opts = minimist(process.argv.slice(2));
 
 if (opts['strict-ssl'] === false) {
-  opts.strictSSL = false
+	opts.strictSSL = false;
 }
 
 download(opts, function (err, zipPath) {
-  if (err) throw err
-  console.log('Downloaded zip:', zipPath)
-  process.exit(0)
-})
+	if (err) {
+		throw err;
+	}
+	console.log('Downloaded zip:', zipPath);
+	process.exit(0);
+});
