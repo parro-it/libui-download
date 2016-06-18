5d7a36c40f168ba647d06b893cd63dbc196098c5
Andrea Parodi <andrea@parro.it>
2016-06-18 18:56:44 +0200

    ported tests to ava

--- test.js
+++ test.js
@@ -1,11 +1,27 @@
-var download = require('./')
+import test from 'ava';
+import pify from 'pify';
+import pathExists from 'path-exists';
 
-download({
-  version: '0.25.1',
-  arch: 'ia32',
-  platform: 'win32'
-}, function (err, zipPath) {
-  if (err) throw err
-  console.log('OK! zip:', zipPath)
-  process.exit(0)
-})
+import _download from './';
+
+const download = pify(_download);
+
+test('Download a release', async t => {
+	const zipPath = await download({
+		version: '0.25.1',
+		arch: 'ia32',
+		platform: 'win32'
+	});
+
+	t.true(pathExists(zipPath));
+});
+
+test('Throws error on 404', async t => {
+	const ex = t.throws(() => download({
+		version: '0.25.1',
+		arch: 'ia32',
+		platform: 'win32'
+	}));
+
+	t.is(ex.message, 'Failed to find Electron v0.25.1 for darwin-ia32 at https://github.com/electron/electron/releases/download/v0.25.1/electron-v0.25.1-darwin-ia32.zip');
+});
