2922dc73fd4be371d2a2f29b2e6433f8aa34048c
Andrea Parodi <andrea@parro.it>
2016-06-22 00:01:58 +0200

    WIP

--- cli.js
+++ cli.js
@@ -1,21 +1,18 @@
 #!/usr/bin/env node
-var read = require('fs').createReadStream;
-var unpack = require('tar-pack').unpack;
+var join = require('path').join;
+var targz = require('./_targz');
 var download = require('./');
 
 download({version: process.argv[2]})
 	.then(function (zipPath) {
 		console.log('Downloaded zip:', zipPath);
-		read(zipPath)
-			.pipe(unpack(process.cwd(), function (err) {
-				if (err) {
-					return console.error(err.stack);
-				}
-				console.log('Libui binaries extracted to:', process.cwd());
-			}));
+		return targz(zipPath, join(process.cwd(), 'tmp'));
+	})
+	.then(function () {
+		console.log('Libui binaries extracted to:', process.cwd());
 	})
 	.catch(function (err) {
-		console.error(err.message);
+		console.error(err.stack);
 		process.exit(1);
 	});
 
