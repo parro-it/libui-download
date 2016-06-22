b7f2e7881f8417a70c80040391c88e5ac706f90d
Andrea Parodi <andrea@parro.it>
2016-06-22 09:30:47 +0200

    fix varios

--- cli.js
+++ cli.js
@@ -1,16 +1,13 @@
 #!/usr/bin/env node
-var join = require('path').join;
-var download = require('./');
 var exec = require('child_process').execSync;
-
+var download = require('./');
 
 download({version: process.argv[2]})
 	.then(function (zipPath) {
 		console.log('Downloaded zip:', zipPath);
-		exec('tar -xzvf ' + zipPath + ' .', { stdio: 'inherit' });
+		exec('tar -xzvf ' + zipPath, {stdio: 'inherit'});
 	})
 	.then(function () {
-
 		console.log('Libui binaries extracted to:', process.cwd());
 	})
 	.catch(function (err) {
