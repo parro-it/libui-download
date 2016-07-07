0f3f47bed0da029c451d187cf10812b468223293
Andrea Parodi <andrea@parro.it>
2016-07-08 01:17:54 +0200

    closes #2

--- cli.js
+++ cli.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-var exec = require('child_process').execSync;
+
+var targz = require('tar.gz');
 var download = require('./');
 
 download({version: process.argv[2]})
 	.then(function (zipPath) {
 		console.log('Downloaded zip:', zipPath);
-		exec('tar -xzvf ' + zipPath, {stdio: 'inherit'});
+		return targz().extract(zipPath, '.');
 	})
 	.then(function () {
 		console.log('Libui binaries extracted to:', process.cwd());
